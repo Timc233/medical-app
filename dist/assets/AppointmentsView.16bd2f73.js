da2fb19a04c0f25432b7f7ac7d2837c0bde2c66b
Timc233 <43799803+Timc233@users.noreply.github.com>
2022-05-08 21:44:35 -0400

    Deploy to pages

---
+++ dist/assets/AppointmentsView.16bd2f73.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,c as t,e,F as s}from"./index.a269e951.js";import{_}from"./NavBar.d55a8cb5.js";const a={};function m(n,c){return r(),t("h1",null,"Appointments")}var p=o(a,[["render",m]]);const d={setup(n){return(c,f)=>(r(),t(s,null,[e(_),e(p,{"mt-3rem":""})],64))}};export{d as default};
